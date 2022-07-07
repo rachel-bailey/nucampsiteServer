b5c11b05c257cd326b663fc3da979aec40f940c0
Rachel Bailey <rachel.ebailey@yahoo.com>
2022-07-06 20:31:26 -0400

    added fileUpload

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require('mongoose');
-const { networkInterfaces } = require('os');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -27,6 +28,15 @@ connect.then(() => console.log("Connected correctly to server"),
 );
 
 var app = express();
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,6 +58,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
